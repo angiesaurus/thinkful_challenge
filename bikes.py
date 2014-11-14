2bf516bb94afaf5e5c08d6f6971101fc98cefb38
Angiesaurus <angelicacoleman3@gmail.com>
2014-11-13 20:19:48 -0800

    updated

--- bikes.py
+++ bikes.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python
-
-import random
-
 class Bicycle(object):
     def __init__(self, model_name, weight, production_cost):
 		self.model_name = model_name
@@ -12,19 +9,6 @@ class Bicycle(object):
     def set_sale_price(self, price):
         self.sale_price = price
 
-	def __str__(self):
-		return self.model_name
-
-	def __repr__(self):
-		return self.model_name
-
-bike1 = Bicycle('Cruiser', 10, 400.00)
-bike2 = Bicycle('Anti-Cruister', 40, 655.00)
-bike3 = Bicycle('Uphill Tank', 8, 150.00)
-bike4 = Bicycle('Fixed Fixie', 13, 800.00)
-bike5 = Bicycle('Lazy Cat', 10, 545.00)
-bike6 = Bicycle('Torpedo', 11, 1275.00)
-
 class BikeShop(object):
 	profit_margin = .20 
 	def __init__(self, name):
@@ -34,15 +18,8 @@ class BikeShop(object):
 		self.profit = 0
 		self.inventory = {}
 
-	def __str__(self):
-		return self.name
-
-	def __repr__(self):
-		return self.name
-
-	def bike_cost(self, bike):
+	def set_bike_price(self, bike):
 		bike.sale_price = bike.production_cost * (1 + self.profit_margin)
-		return bike.sale_price
 		
 	def add_customer(self, customer):
 		self.customers.append(customer)
@@ -63,7 +40,7 @@ class BikeShop(object):
 	def available_bikes(self, max_costs):
 		affordable_bikes = []
 		for bike in self.bikes:
-			if self.bike_cost(bike) <= max_costs:
+			if self.set_bike_price(bike) <= max_costs:
 				affordable_bikes.append(bike)
 		return affordable_bikes
 
@@ -74,31 +51,30 @@ class BikeShop(object):
 			self.profit += bike.sale_price - bike.production_cost
 
 
-angies_shop = BikeShop('angies_shop')
-angies_shop.add_bike(bike1)
-angies_shop.add_bike(bike2)
-angies_shop.add_bike(bike3)
-angies_shop.add_bike(bike4)
-angies_shop.add_bike(bike5)
-angies_shop.add_bike(bike6)
-
 class Customer(object):
 	def __init__(self, name, available_funds):
 		self.name = name
 		self.available_funds = available_funds
 		self.bikes = []
 
-	def __str__(self):
-		return self.name
-
-	def __repr__(self):
-		return self.name
-
 	def buy_bike(self, bike):
 		self.available_funds = self.available_funds - bike.sale_price
 		self.bikes.append(bike)
 		
-        
+bicycles = [
+Bicycle('Cruiser', 10, 400.00),
+Bicycle('Anti-Cruister', 40, 655.00),
+Bicycle('Uphill Tank', 8, 150.00),
+Bicycle('Fixed Fixie', 13, 800.00),
+Bicycle('Lazy Cat', 10, 545.00),
+Bicycle('Torpedo', 11, 1275.00)
+]
+
+angies_shop = BikeShop('angies_shop')
+
+for bike in bicycles:
+	angies_shop.add_bike(bike)
+
 customers = ["Bob","Mary","Jack"]
 dollars = [200.00,500.00,1000.00]
 
@@ -114,7 +90,7 @@ print
 
 print "The following customers are looking to buy a bike: "
 for customer in angies_shop.customers:
-	print customer
+	print customer.name
 print
 
 for customer in angies_shop.customers:
@@ -128,7 +104,3 @@ for customer in angies_shop.customers:
 		print "%s:\t%s" % (k,v)
 	print "Angie's shop has made $%.2f" % angies_shop.total_profit()
 	print 
-
-# print angies_shop.get_inventory()
-# print angies_shop.total_profit()
-
